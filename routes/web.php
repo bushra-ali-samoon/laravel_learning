57a672db59d96d871aaef9150f6846420879494c
Bushra Ali <99665258+bushra-ali-samoon@users.noreply.github.com>
2025-05-16 10:33:44 +0500

    blade with examples

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\loginController;
 // });
 // // ,
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // // Route::get('/about', function () {
 // //     return view('about');
